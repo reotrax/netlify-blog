e7408e8b1670e2143e617b9300bcebec98ec447f
reotrax <reo.trax127@gmail.com>
2021-11-12 20:45:53 +0900

    記事を見れる様に回収中

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,65 @@
-<template>
+<template lang="pug">
   <Layout>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    h1 Hello, world!
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    p
+      | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
 
-    <p class="home-links">
+    p.home-links
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+
+    h1 Gridsomeで作るブログ
+
+    h2 投稿一覧
+
+    .kiji(
+      v-for="post in $page.posts.edges" :key="post.node.id"
+    )
+      | タイトル: 
+      g-link(:to="post.node.path")
+        | {{ post.node.title }}
+
+      p.gaiyo
+        | 概要: {{ post.node.description }}
+
+      p.tag
+        | タグ: {{ post.node.tags }}
+      ul(
+        v-if="post.node.tags"
+      )
+        li(
+          v-for="tag in post.node.tags"
+          :key="tag.id"
+        )
+          | {{ tag }}
 
   </Layout>
 </template>
+
+<page-query>
+{
+  posts: allPost {
+    edges {
+      node {
+        path
+        title
+        tags {
+          id
+          title
+          path
+        }
+        description
+        content
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
@@ -26,8 +69,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .home-links a {
   margin-right: 1rem;
+}
+
+p {
+  margin: 0;
+}
+
+.kiji {
+  margin: 20px 0 0;
+  background-color: lightgray;
+  ul {
+    margin: 0;
+    li {
+
+    }
+  }
+  &:nth-of-type(1) {
+    margin: 0;
+  }
 }
 </style>
